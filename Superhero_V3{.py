5d4597e2770a3508cfa9e323e89ebb95ccc0807f
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-20 10:37:09 +1300

    Update Superhero_V3{.py

--- Superhero_V3{.py
+++ Superhero_V3{.py
@@ -1,13 +1,18 @@
 hero_0 = {
     "Name": "superman",
     "Power": "super strength", 
-    "Strength": 100
+    "Strength": 100,
+    "Speed": 120,
 }
+
 print(hero_0)
 
 hero_0["speed"] = 120
 print(hero_0)
 
+print(hero_0["Name"])
+print(hero_0["Power"])
+print(hero_0["Strength"])
 
 if "Name" in hero_0:
     print("The superhero's name is:", hero_0["Name"])
@@ -23,5 +28,6 @@ if "Strength" in hero_0:
     print("The superhero's strength is:", hero_0["Strength"])
 else:
     print("There is no superhero strength.")
+
 
 
