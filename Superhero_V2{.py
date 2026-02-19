682cced1e0b08a300eb91ce21b6a4fddece000ac
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-20 10:30:01 +1300

    Update Superhero_V2{.py

--- Superhero_V2{.py
+++ Superhero_V2{.py
@@ -17,3 +17,13 @@ if "Power" in hero_0:
     print("The superhero's power is:", hero_0["Power"])
 else:
     print("There is no superhero power.")
+
+if "superman" in hero_0["name"]:
+    print("That is the superhero's name.")
+else:
+    print("That isn't the superhero's name.")
+
+if "kryptonite" in hero_0["name"]:
+    print("That is the superhero's name.")
+else:
+    print("That isn't the superhero's name.")
