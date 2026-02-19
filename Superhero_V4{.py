f82be487c57baf5f29a4a4b4195339c9206f2126
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-20 10:44:28 +1300

    Create Superhero_V4{.py

---
+++ Superhero_V4{.py
@@ -0,0 +1,34 @@
+hero_0 = {
+    "Name": "superman",
+    "Power": "super strength", 
+    "Strength": 100,
+}
+
+if "Name" in hero_0:
+    print("The superhero's name is:", hero_0["Name"])
+else:
+    print("There is no superhero name.")
+
+if "Power" in hero_0:
+    print("The superhero's power is:", hero_0["Power"])
+else:
+    print("There is no superhero power.")
+
+if "Strength" in hero_0:
+    print("The superhero's strength is:", hero_0["Strength"])
+else:
+    print("There is no superhero strength.")
+
+print(hero_0)
+
+hero_0["speed"] = 120
+print(hero_0)
+
+hero_0["Strength"] = 100
+print(hero_0)
+
+popped_item = hero_0.pop("Power")
+print(hero_0)
+print(popped_item)
+
+
