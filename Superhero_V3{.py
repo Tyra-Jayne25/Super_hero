df8ce57aaf07e36d17fdec8fee8f6c0d6cbcddab
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-20 10:34:47 +1300

    Create Superhero_V3{.py

---
+++ Superhero_V3{.py
@@ -0,0 +1,27 @@
+hero_0 = {
+    "Name": "superman",
+    "Power": "super strength", 
+    "Strength": 100
+}
+print(hero_0)
+
+hero_0["speed"] = 120
+print(hero_0)
+
+
+if "Name" in hero_0:
+    print("The superhero's name is:", hero_0["Name"])
+else:
+    print("There is no superhero name.")
+
+if "Power" in hero_0:
+    print("The superhero's power is:", hero_0["Power"])
+else:
+    print("There is no superhero power.")
+
+if "Strength" in hero_0:
+    print("The superhero's strength is:", hero_0["Strength"])
+else:
+    print("There is no superhero strength.")
+
+
