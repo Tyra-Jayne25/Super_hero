43d960eecd6b06730277d084771b7d2d8362b3dc
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-23 13:48:14 +1300

    Update Superhero_V6{.py

--- Superhero_V6{.py
+++ Superhero_V6{.py
@@ -1,30 +1,30 @@
 hero_0 = {
-    "Name": "superman",
-    "Power": "super strength", 
-    "Strength": 100,
+    "name": "superman",
+    "power": "super strength", 
+    "strength": 100,
 }
 
 hero_1 = {
-    "Name": "batman",
-    "Power": "intellect", 
-    "Strength": 85,
-    "Speed": 80
+    "name": "batman",
+    "power": "intellect", 
+    "strength": 85,
+    "speed": 80
 }
 hero_list = [hero_0, hero_1]
 print (hero_list)
 
-if "Name" in hero_0:
-    print("The superhero's name is:", hero_0["Name"])
+if "name" in hero_0:
+    print("The superhero's name is:", hero_0["name"])
 else:
     print("There is no superhero name.")
 
-if "Power" in hero_0:
-    print("The superhero's power is:", hero_0["Power"])
+if "power" in hero_0:
+    print("The superhero's power is:", hero_0["power"])
 else:
     print("There is no superhero power.")
 
-if "Strength" in hero_0:
-    print("The superhero's strength is:", hero_0["Strength"])
+if "strength" in hero_0:
+    print("The superhero's strength is:", hero_0["strength"])
 else:
     print("There is no superhero strength.")
 
@@ -33,7 +33,7 @@ print(hero_0)
 hero_0["speed"] = 120
 print(hero_0)
 
-hero_0["Strength"] = 100
+hero_0["strength"] = 100
 print(hero_0)
 
 print("Superhero 1 stats \n")
@@ -44,4 +44,5 @@ print("Superhero 2 stats \n")
 for i in hero_1:
     print(i, ":", hero_1[i])
 
-
+for i in hero_list:
+    print(i["name"] + "'s superpower is", i["power"], "and their strength value is", i["strength"])
