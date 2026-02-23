4ffc8d77a05c128098b475631c47f5e6d358d114
Tyra-Jayne25 <tyra.jayne25@gmail.com>
2026-02-23 14:16:09 +1300

    Update Superhero_V8{.py

--- Superhero_V8{.py
+++ Superhero_V8{.py
@@ -15,3 +15,24 @@ for hero_id, hero_info in heroes.items():
 
     for key in hero_info:
         print(key + ":", hero_info[key])
+
+heroes = {}
+
+for i in range(2):
+    ID = input("\n\nEnter ID: ")
+    heroes[ID] = {}
+
+    name = input("Enter name: ")
+    heroes[ID] ["name"] = name
+
+    power = input("Enter power: ")
+    heroes[ID] ["power"] = power
+
+    strength = int(input("Enter strength: "))
+    heroes[ID] ["strength"] = strength
+
+    speed = int(input("Enter speed: "))
+    heroes[ID] ["speed"] = speed
+
+    print("\n\n")
+    print(heroes)
